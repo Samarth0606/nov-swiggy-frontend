d2a0ca3c0e0953f59ed5a9b61752e5afd5819d13
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-06-01 21:05:18 +0530

    swiggy-fe-deploy

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,7 +7,7 @@ function Login(props) {
   const [isSignUp, setIsSignUp] = useState(true);
 
   function handleRegister() {
-    const response = fetch("http://localhost:8000/api/register", {
+    const response = fetch("https://nov-swiggy-backend.onrender.com/api/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -30,7 +30,7 @@ function Login(props) {
   }
 
   function handleLogin() {
-    const response = fetch("http://localhost:8000/api/login", {
+    const response = fetch("https://nov-swiggy-backend.onrender.com/api/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
